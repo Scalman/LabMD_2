7d712be0e99a16ce15b916cc46c81561c2ce75fb
Scalman <waleedhassan@live.se>
2016-01-26 23:00:54 +0100

    new project with MD

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage){
-
+//sjdjdjdjdj
         try {
             InitFxml initFxml = new InitFxml();
             initFxml.initializeLoadersAndControllers(primaryStage);
